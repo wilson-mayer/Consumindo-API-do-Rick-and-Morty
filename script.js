a0addbd08dae0d5f750a3dea93f5310b38484a97
Wilson <wr.mayer@live.com>
2024-04-16 09:36:02 -0300

    add: last episode and status color

--- script.js
+++ script.js
@@ -39,15 +39,17 @@ function getCharacters(page = 1) {
         const characterCard = document.createElement("div");
         characterCard.classList.add("character-card");
         const details = character.url;
+        const lastEpisodeUrl = character.episode.pop();
 
         characterCard.innerHTML = `
         <div class="card bg-transparent ">
             <img src='${character.image}' alt='' class="card-img-top">
-            <div class="card-body bg-transparent border border-3 border-top-0 border-success rounded-bottom">
+            <div class="card-body py-4 px-5 bg-transparent border border-3 border-top-0 border-success rounded-bottom">
             <a  class="card-title fw-bold fs-2 text-decoration-none namehover lh-1" id="abrirModal${character.id}">
             ${character.name}</a>
-            <p class="card-text text-white lh-base fs-5">${character.status} - ${character.species}</p>
+            <p class="card-text d-flex align-items-center text-white lh-base fs-5"><span class="${character.status}"></span>${character.status} - ${character.species}</p>
             <p class="card-text text-white-50 fs-6 lh-1">Ultima localização conhecida:</p><p class="card-text text-white fs-5">${character.location.name}</p>
+            <p class="card-text text-white-50 fs-6 lh-1">Último episódio visto:</p><p class="card-text text-white fs-5" id="last-seen-${character.id}" ></p>
             </div>
         </div>
           `;
@@ -67,7 +69,7 @@ function getCharacters(page = 1) {
                         <div class="modal-body colormodal d-flex justify-content-center">
                         <div class="card bg-transparent cardModal">
                         <img  src='${character.image}' alt='' class="card-img-top">
-                        <div class="card-body bg-transparent border border-3 border-top-0 border-success rounded-bottom">
+                        <div class="card-body py-4 px-5 bg-transparent border border-3 border-top-0 border-success rounded-bottom">
                         <a href="${details}" target="_blank" class="card-title fw-bold fs-1 text-decoration-none namehovermodal fontmodal lh-1">${character.name}</a>
                         <p class="card-text text-white lh-base fs-4 ">Status: ${character.status}</p> 
                         <p class="card-text text-white lh-base fs-4">Espécie: ${character.species} </p>
@@ -89,9 +91,22 @@ function getCharacters(page = 1) {
             var myModal = new bootstrap.Modal(myModalEl);
             myModal.show();
           });
+
+        axios
+          .get(lastEpisodeUrl)
+          .then(function (episode) {
+            const labelEpisode = document.getElementById(
+              `last-seen-${character.id}`
+            );
+            const nameEpisode = episode.data.name;
+            labelEpisode.innerHTML = nameEpisode;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       });
 
-      pageNumber.innerHTML = `<p class="page-link bg-transparent text-warning fw-bold" >${page}</p>`;
+      pageNumber.innerHTML = `<p class="page-link bg-transparent text-warning fw-bold border border-success" >${page}</p>`;
 
       console.log(allCharacters);
     })
@@ -210,6 +225,7 @@ document
         resultsModal.show();
       })
       .catch((error) => {
+        alert("Personagem não encontrado!");
         console.error("Erro na busca:", error);
       });
   });
